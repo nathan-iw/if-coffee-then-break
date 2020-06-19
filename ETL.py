ee22b99b62b9c39a6d43da66c172f6349579b9ac
Alex <Alexander.Wallwork@infinityworks.com>
2020-06-19 10:53:18 +0100

    adapted for cron (every min), fixed for RDS inputs, made independent RDS transform file, fixed up logger

--- ETL.py
+++ ETL.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python3
+
 import pymysql
 from os import environ
 import time
-# from extract import Extract
-from csv_extract import Extract
-from transform import Transform
+from extract import Extract
+# from csv_extract import Extract
+from transform_rds import Transform
+# from transform import Transform
 from load import Load
 from log import logger
          
@@ -14,9 +17,11 @@ if __name__ == "__main__":
     start = time.time()
     app = Extract()
     # Command to extract data from csv via s3 bucket:
-    raw_data_list = app.get_data_from_bucket("transactions/20200611132822.csv")
+    # raw_data_list = app.get_data_from_bucket("transactions/20200611132822.csv")
     # Commands to load data from RDS:
-    # raw_data_list = app.load_yesterdays_data() # extract output from yesterday
+    raw_data_list = app.load_a_min() # extract output from yesterday
+    #  raw_data_list = app.load_yesterdays_data() # extract output from yesterday
+
     # raw_data_list = app.load_all_data()  # extract output from all time
     end_extract = time.time()
     extract_time = round(end_extract - start, 4)
